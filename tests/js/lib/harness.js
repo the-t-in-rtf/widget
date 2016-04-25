d21384083ed56cd90daef0869ef0c78b61c01914
Tony Atkins <tony@raisingthefloor.org>
2016-04-25 17:22:12 +0200

    UL-79:  Updated `harness` after quick conversation with Antranig.

--- tests/js/lib/harness.js
+++ tests/js/lib/harness.js
@@ -9,19 +9,7 @@ var fluid = require("infusion");
 
 require("ul-api");
 
-
-// gpii.ul.api.loadTestingSupport();
-/*
-    If we load the test harness using the above code, we end up with errors like:
-
-    11:40:03.440:  jq: ***************
-    11:40:03.440:  jq: Failure in fixture file - no tests were queued - FAIL
-    11:40:03.440:  jq: ***************
-
-    TODO:  Review with Antranig and remove workaround below
- */
-
-require(fluid.module.resolvePath("%ul-api/tests/js/lib/test-harness.js"));
+fluid.require("%ul-api/tests/js/lib/test-harness.js");
 
 // The API test harness with our local templates and code
 fluid.defaults("gpii.test.userReviewWidget.harness", {
