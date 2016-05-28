d6551619ff5bf75cae682889f3a838b156789c3c
Pablo Suárez León <psuarez@consultoria.ilunion.com>
2016-05-28 16:11:00 +0200

    Initialization tests

--- test/widget.js
+++ test/widget.js
@@ -1,3 +1,44 @@
 describe('Widget', function(){
 	
+	beforeEach(function() {
+		jasmine.Ajax.install();
+		spyOn(WidgetUI, 'setWidgetState').and.callThrough();
+		spyOn(WidgetAPI, 'getAverageAndComments').and.callThrough();
+		spyOn(WidgetAPI, 'doRequest').and.callThrough();
+		spyOn($, 'ajax').and.callFake(function(options){
+			options.success({value:3,comments:[]});
+		});
+		spyOn(WidgetUI, 'setWidgetStateCallback');
+		WidgetUI.initWidget();
+	});
+
+	afterEach(function() {
+		jasmine.Ajax.uninstall();
+	});
+	
+	describe('Initialization', function(){
+
+		it('Should call WidgetUI.setWidgetState', function(){
+			expect(WidgetUI.setWidgetState).toHaveBeenCalled();
+		});
+		
+		it('Should call WidgetAPI.getAverageAndComments', function(){
+			expect(WidgetAPI.getAverageAndComments).toHaveBeenCalledWith(undefined, WidgetUI.setWidgetStateCallback);
+		});
+		
+		it('Should call WidgetAPI.doRequest', function(){
+			var preferences = {
+				'id':WidgetConf.id,
+				'app':WidgetConf.app,
+				'user':WidgetConf.user,
+				'grvalue':undefined
+			};
+			expect(WidgetAPI.doRequest).toHaveBeenCalledWith('ControllerGetStar', preferences, WidgetUI.setWidgetStateCallback);
+		});
+		
+		it('Should call WidgetUI.setWidgetStateCallback', function(){
+			expect(WidgetUI.setWidgetStateCallback).toHaveBeenCalledWith({value:3,comments:[]});
+		});
+		
+	});
 });
