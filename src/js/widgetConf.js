892ead3db1414b72fa1ff4cf04451219cf4bf07b
Pablo Suárez León <psuarez@consultoria.ilunion.com>
2016-07-28 11:08:00 +0200

    Configuration correction

--- src/js/widgetConf.js
+++ src/js/widgetConf.js
@@ -1,6 +1,6 @@
 var WidgetConf = {
     //url:  'http://localhost:8084/widget/',
-    url:  'http://193.27.9.220/widget/q',
+    url:  'http://193.27.9.220/widget/',
     app:  'ul',
     user: '4',
     id:   $(location).attr('pathname'),
